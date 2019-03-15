e19f4d7642e460a98ae4b6474d55bfd74fad0f65
Xavizardon <00013812@uca.edu.sv>
2019-03-15 16:37:02 -0600

    first commit

---
+++ app/src/main/java/com/example/ejer1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ejer1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnclck;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
