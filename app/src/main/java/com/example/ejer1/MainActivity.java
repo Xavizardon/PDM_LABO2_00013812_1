2bcf40f8f7929165073106aa9c2ef107a1abb74b
Xavizardon <00013812@uca.edu.sv>
2019-03-15 16:57:06 -0600

    Agregado metodos, aun por probar

--- app/src/main/java/com/example/ejer1/MainActivity.java
+++ app/src/main/java/com/example/ejer1/MainActivity.java
@@ -2,18 +2,44 @@ package com.example.ejer1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener, View.OnClickListener {
 
     private Button btnclck;
+
+    private TextView texto;
+
+    private EditText username, pssword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnclck = findViewById(R.id.botonazo);
+        texto = findViewById(R.id.textito);
+        username = findViewById(R.id.user_edit);
+        pssword = findViewById(R.id.psswrd_edit);
+
+        btnclck.setOnClickListener(this);
+        btnclck.setOnLongClickListener(this);
+
+    }
 
 
+    @Override
+    public boolean onLongClick(View v) {
+        texto = pssword;
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        texto = username;
     }
 }
